68444cc6d48a2c9c76cbc70c5d85ac6a776aa448
jorgenfoss <jorgen.foss@gmail.com>
2017-09-23 10:55:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ cd ${HOME}/configs
 [ -f ${HOME}/.vimrc ] && mv ${HOME}/.vimrc ${HOME}/configs/.vimrc_old
 [ -f ${HOME}/.bashrc ] && mv ${HOME}/.bashrc ${HOME}/configs/.bashrc_old
 [ -f ${HOME}/.dircolors ] && mv ${HOME}/.dircolors ${HOME}/configs/.dircolors_old
+[ -f ${HOME}/.tmux.conf ] && mv ${HOME}/.bashrc ${HOME}/configs/.tmux.conf_old
 
 # Create links to new files
 if [ ! -L ${HOME}/.vimrc ]; then 
@@ -16,6 +17,9 @@ if [ ! -L ${HOME}/.bashrc ]; then
 fi
 if [ ! -L ${HOME}/.dircolors ]; then 
     ln -s ${HOME}/configs/.dircolors ${HOME}/.dircolors 
+fi
+if [ ! -L ${HOME}/.tmux.conf ]; then 
+    ln -s ${HOME}/configs/.tmux.conf ${HOME}/.tmux.conf 
 fi
 
 echo "Done"
